d3bf00e460fa4bda5b9e1a9d386faeedc74ad532
Cy\Cy <marion.lauzier.ad@gmail.com>
2025-05-05 20:51:05 +0200

    connection controller fix

--- src/main/java/com/openclassrooms/paymybuddy/service/ConnectionService.java
+++ src/main/java/com/openclassrooms/paymybuddy/service/ConnectionService.java
@@ -1,31 +1,31 @@
-//package com.openclassrooms.paymybuddy.service;
-//
-//import com.openclassrooms.paymybuddy.model.User;
-//import com.openclassrooms.paymybuddy.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Optional;
-//
-//@Service
-//public class ConnectionService {
-//
-//    @Autowired
-//    UserRepository userRepository;
-//
-//    public boolean addConnection(String currentUserEmail, String email) {
-//
-//        Optional<User> currentUser = userRepository.findByEmail(currentUserEmail).orElseThrow(() -> new RuntimeException("Current user not found"));
-//        User userToConnect = userRepository.findByEmail(email);
-//
-//        if (userRepository.existsByEmail(email)) {
-//            if (currentUser.getConnections().contains(userToConnect)) {
-//                return false;
-//            }
-//            currentUser.getConnections().add(userToConnect);
-//            userRepository.save(currentUser);
-//            return true;
-//        }
-//        return false;
-//    }
-//}
+package com.openclassrooms.paymybuddy.service;
+
+import com.openclassrooms.paymybuddy.model.User;
+import com.openclassrooms.paymybuddy.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ConnectionService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public boolean addConnection(String currentUserEmail, String email) {
+
+        User currentUser = userRepository.findByEmail(currentUserEmail).orElseThrow(() -> new RuntimeException("Current user not found"));
+        User userToConnect = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("The user you want to connect to was not found"));
+
+        if (userRepository.existsByEmail(email)) {
+            if (currentUser.getConnections().contains(userToConnect)) {
+                return false;
+            }
+            currentUser.getConnections().add(userToConnect);
+            userRepository.save(currentUser);
+            return true;
+        }
+        return false;
+    }
+}
