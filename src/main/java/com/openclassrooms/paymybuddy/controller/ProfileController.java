a3a3b28cfc79fff548fe75b877922433a583e987
Cy\Cy <marion.lauzier.ad@gmail.com>
2025-05-15 10:22:50 +0200

    added logs in ProfileController

--- src/main/java/com/openclassrooms/paymybuddy/controller/ProfileController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controller/ProfileController.java
@@ -2,6 +2,8 @@ package com.openclassrooms.paymybuddy.controller;
 
 import com.openclassrooms.paymybuddy.model.User;
 import com.openclassrooms.paymybuddy.repository.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
@@ -13,16 +15,25 @@ import java.security.Principal;
 @Controller
 public class ProfileController {
 
+    private static final Logger logger = LogManager.getLogger("ProfileController");
+
     @Autowired
     UserRepository userRepository;
 
     @GetMapping("/profile")
     public String showProfile(Model model, Principal principal) {
 
+        logger.info("GET request to /profile");
+
         String email = principal.getName();
 
         User user = userRepository.findByEmail(email)
-                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                 .orElseThrow(() -> {
+                     logger.error("User with email '{}' not found in database.", email);
+                     return new UsernameNotFoundException("User not found");
+                 });
+
+        logger.info("User '{}' profile loaded successfully.", email);
 
         model.addAttribute("user", user);
         return "profile";
